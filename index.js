5d1040ae222de2b7be9b7a1b20764127a639672d
Alessandro Krepk <alessandro1@devgmail.com>
2024-09-13 19:21:20 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const listarMetas = async () => {
     choices: metas.map(meta => ({ name: meta.value, value: meta.value, checked: meta.checked })),
     instructions: false,
   })
-  
+
   metas.forEach((m) => {
     m.checked = false
   })
@@ -56,8 +56,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas Realizadas",
+    message: "Metas Realizadas: " + realizadas.length,
     choices: [...realizadas]
+  })
+}
+
+const metasAbertas = async () => {
+  const abertas = metas.filter ((meta) => {
+    return meta.checked != true
+  })
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas! :)")
+    return
+  }
+
+  await select ({
+    message: "Metas Abertas: " + abertas.length,
+    choices: [...abertas]
   })
 }
 
@@ -79,6 +95,10 @@ const start = async () => {
           value: "realizadas"
         },
         {
+          name: "Metas abertas",
+          value: "abertas"
+        },
+        {
           name: "Sair",
           value: "sair"
         }
@@ -96,6 +116,9 @@ const start = async () => {
       case "realizadas":
         await metasRealizadas()
         break
+      case "abertas":
+        await metasAbertas()
+         break
       case "sair":
         console.log("Até a próxima!")
         return
